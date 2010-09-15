2d1c824e59968436935c66eafa66bb8d97c8eaa8
Matthew Kocher <kocher@gmail.com>
2010-09-15 07:40:35 -0700

    test repo

--- add_builds.rb
+++ add_builds.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 require 'environment'
 
-build = Build.find_or_create(:github_user => "pivotalexeperimental", :github_repository => "wschef", :git_branch => "master")
-build.run_script = "ls -l /"
+build = Build.find_or_create_by_github_user_and_github_repository_and_git_branch(:github_user => "mkocher", :github_repository => "touchable", :git_branch => "master")
+build.run_script = "ls -l /; date;"
 build.save
